73d77ed189003a132596333c1ae6085e0f7b0df7
slowchessblitz <slowchessblitz@gmail.com>
2021-07-26 23:25:39 +0530

    Subtraction code added

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,9 @@ public class Calculator{
 	public static void add(int x,int y){
 		System.out.println("Addition is "+(x+y));
 	}
+	public static void sub(int x,int y){
+		System.out.println("Addition is "+(x-y));
+	}
 	public static void main(String[]args){
 		
 	}
