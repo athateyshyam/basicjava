38e71a4880ffcfdcd7656ec6d7ef731dc33f1fa5
athateyshyam <athateyshyam@gmail.com>
2021-07-27 13:03:38 +0530

    Division method is added

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,13 @@ public class Calculator{
 		System.out.println("Addition is "+(x+y));
 	}
 	public static void sub(int x,int y){
-		System.out.println("Addition is "+(x-y));
+		System.out.println("Subtraction is "+(x-y));
 	}
 	public static void mul(int x,int y){
-		System.out.println("Addition is "+(x*y));
+		System.out.println("Multiplication is "+(x*y));
+	}
+	public static void div(int x,int y){
+		System.out.println("Division is "+(x/y));
 	}
 	public static void main(String[]args){
 		
